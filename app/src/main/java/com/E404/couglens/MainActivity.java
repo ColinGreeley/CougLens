cd02d70d100c2e6eba41270d3c509e188edd8f23
Kyle Newotn <kyle.e.newton@wsu.edu>
2019-02-23 17:50:22 -0800

    Upload Picture from Camera Roll

--- app/src/main/java/com/E404/couglens/MainActivity.java
+++ app/src/main/java/com/E404/couglens/MainActivity.java
@@ -9,57 +9,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final int RESULT_LOAD_IMAGE = 1;
-    ImageView imageToUpload;
-    Button Button2;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
-        Button2 = (Button) findViewById(R.id.Button2);
-        imageToUpload.setOnClickListener(this);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own ", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-
-        //Button takePickture =  findViewById(R.id.Button1);
     }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.imageToUpload:
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
-
-                break;
-
-        }
-
-    }
-
 
     static final int REQUEST_TAKE_PHOTO = 1;
     public void openCamera(View view) {
@@ -70,37 +35,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void openUpload(View view) {
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
-            Uri selectedImage = data.getData();
-            imageToUpload.setImageURI(selectedImage);
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if(galleryIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
         }
     }
 }
