9985289e4e3e65fb2264b263f7a717cb26d1ea40
Kyle Newotn <kyle.e.newton@wsu.edu>
2019-02-24 01:43:56 -0800

    Cleaned up and merged code

--- app/src/main/java/com/E404/couglens/MainActivity.java
+++ app/src/main/java/com/E404/couglens/MainActivity.java
@@ -1,21 +1,10 @@
 package com.E404.couglens;
 
-import android.Manifest;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,5 +35,9 @@ public class MainActivity extends AppCompatActivity {
     public void openRecent(View view) {
        Intent recentIntent = new Intent(this, RecentSearchActivity.class);
        startActivity(recentIntent);
+    }
+
+    public void openInfo(View view) {
+
     }
 }
