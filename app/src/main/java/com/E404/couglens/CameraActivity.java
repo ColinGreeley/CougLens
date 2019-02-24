20c81f913439de93369064c1ec7b53cf22de84cb
Kyle Newton <herobrine098@gmail.com>
2019-02-23 16:04:34 -0800

    Added Camera Functionality

---
+++ app/src/main/java/com/E404/couglens/CameraActivity.java
@@ -0,0 +1,4 @@
+package com.E404.couglens;
+
+public class CameraActivity {
+}
--- app/src/main/java/com/E404/couglens/MainActivity.java
+++ app/src/main/java/com/E404/couglens/MainActivity.java
@@ -1,13 +1,20 @@
 package com.E404.couglens;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Button takePickture =  findViewById(R.id.Button1);
+    }
+    static final int REQUEST_TAKE_PHOTO = 1;
+    public void openCamera(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
     }
 
     @Override
