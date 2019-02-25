dcd2e883025ca33b73defc48135cf531a952cb24
ColinGreeley <37784147+ColinGreeley@users.noreply.github.com>
2019-02-24 16:36:42 -0800

    added camera functionality

--- app/src/main/java/com/E404/couglens/Clarifai/clarifaiMain.java
+++ app/src/main/java/com/E404/couglens/Clarifai/clarifaiMain.java
@@ -15,13 +15,17 @@ import clarifai2.dto.prediction.Prediction;
 
 public class clarifaiMain {
 
-    final ClarifaiClient client = new ClarifaiBuilder("e4c27199fe5e46dfb0ff50f84d6ee033").buildSync();
+    public ClarifaiResponse<List<ClarifaiOutput<Prediction>>> getResponse(String filePath) {
+        final ClarifaiClient client = new ClarifaiBuilder("e4c27199fe5e46dfb0ff50f84d6ee033").buildSync();
 
-    ConceptModel model = client.getDefaultModels().generalModel();
-    ModelVersion modelVersion = model.getVersionByID("the-version").executeSync().get();
+        ConceptModel model = client.getDefaultModels().generalModel();
+        ModelVersion modelVersion = model.getVersionByID("the-version").executeSync().get();
 
-    ClarifaiResponse<List<ClarifaiOutput<Prediction>>> response = client.predict(model.id())
-            .withInputs(ClarifaiInput.forImage("https://samples.clarifai.com/metro-north.jpg"))
-            .withVersion("aa7f35c01e0642fda5cf400f543e7c40")
-            .executeSync();
+        ClarifaiResponse<List<ClarifaiOutput<Prediction>>> response = client.predict(model.id())
+                .withInputs(ClarifaiInput.forImage(filePath))
+                .withVersion("aa7f35c01e0642fda5cf400f543e7c40")
+                .executeSync();
+
+        return response;
+    }
 }
--- app/src/main/java/com/E404/couglens/MainActivity.java
+++ app/src/main/java/com/E404/couglens/MainActivity.java
@@ -1,10 +1,27 @@
 package com.E404.couglens;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,29 +32,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     static final int REQUEST_TAKE_PHOTO = 1;
+
     public void openCamera(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.E404.couglens",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
+
+
 
     public void openUpload(View view) {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         if(galleryIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
         }
-    }
-
-
-    public void openRecent(View view) {
-       Intent recentIntent = new Intent(this, RecentSearchActivity.class);
-       startActivity(recentIntent);
-    }
-
-    public void openInfo(View view) {
-
     }
 }
